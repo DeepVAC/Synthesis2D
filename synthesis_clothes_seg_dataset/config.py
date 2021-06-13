96f8adac9284e74c53d8d32e8056aa67352a1670
Gemfield <gemfield@civilnet.cn>
2021-06-13 16:49:07 +0800

    merge portrait synthesis and clothes synthesis

--- synthesis_clothes_seg_dataset/config.py
+++ synthesis_clothes_seg_dataset/config.py
@@ -5,7 +5,8 @@ config.num_classes = 2
 # config.skin_model = "weights/skin.pth"
 config.input_size = (448, 448)
 config.portrait_model = "weights/portrait.pth"
-
+#whether to use portrait seg model to generate portriat mask and help to generate final dst mask.
+config.use_portrait_mask = True
 config.multiple = 2
 #sys.argv[1] can override
 config.original_image_label_dir = "/gemfield/hostpv2/deepvac_up_bottom_hat"
